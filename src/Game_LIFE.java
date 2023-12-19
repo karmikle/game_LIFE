baa9995b262c71ed6afbc89102d37708826b7ffe
karmikle <karmikle@gmail.com>
2023-12-19 23:48:39 +0200

    game_LIFE comments added

--- src/Game_LIFE.java
+++ src/Game_LIFE.java
@@ -1,14 +1,14 @@
 public class Game_LIFE {
     public static void main(String[] args) throws InterruptedException {
 
-        int [][] w = new int[20][20];
-        int delay = 500;
+        int [][] w = new int[20][20]; //matrix size
+        int delay = 500; //delay for refresh
         int countDays = 0;
         start(w);
         drawWorld(w);
 
 
-        for (int n = 0; n < 200; n++) {
+        for (int n = 0; n < 200; n++) { //200 - number of cycles
             updateWorld(w);
             drawWorld(w);
 
@@ -19,6 +19,7 @@ public class Game_LIFE {
         }
     }
 
+    // assigning random 0 - 1, => then 0 or 1 if greater than 0.5
     private static void start (int [][] world)
     {
         for ( int i = 0; i < world.length; i++) {
@@ -28,6 +29,8 @@ public class Game_LIFE {
         }
     }
 
+
+    // replacing 0 and 1 on squares (from windows chart instrument)
     private static void drawWorld (int[][]world)
     {
         for (int[] el : world) {
@@ -49,12 +52,17 @@ public class Game_LIFE {
 
     }
 
+    //Creating temporary matrix , size like original
     private static void updateWorld (int[][] world) {
         int[][] cycle = new int[world.length][world[0].length];
 
         for (int i = 0; i < world.length; i ++) {
             for (int j = 0; j < world[i].length; j ++) {
+
+                // using method countCell in order to find the future destiny of the cell
                 int numberOfCells = countCells(world, i, j);
+
+                // using game rules
                 if (world[i][j] == 1) {
                     cycle[i][j] = (numberOfCells == 2 || numberOfCells == 3) ? 1 : 0;
                 } else {
@@ -62,9 +70,12 @@ public class Game_LIFE {
                 }
             }
         }
+
+        // finally replacing previous cycle with new one
         System.arraycopy(cycle,0, world, 0, world.length);
     }
 
+    // method to find out how many adjacent cells are using 9 cases of positioning
     private static int countCells (int[][] world, int line, int col){
         int neibCells = 0;
         if (line == 0 && col == 0)
